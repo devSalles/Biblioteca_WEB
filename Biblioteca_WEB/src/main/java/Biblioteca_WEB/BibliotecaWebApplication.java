ddb567f697910a55e5f8b43e8316d66e614fac03
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2025-06-21 19:43:00 -0300

    Primeiro Commit iniciando repositório

---
+++ Biblioteca_WEB/src/main/java/Biblioteca_WEB/BibliotecaWebApplication.java
@@ -0,0 +1,13 @@
+package Biblioteca_WEB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaWebApplication.class, args);
+	}
+
+}
---
+++ Biblioteca_WEB/src/test/java/Biblioteca_WEB/BibliotecaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package Biblioteca_WEB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
