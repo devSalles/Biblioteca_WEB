990929ac3af9d74abeaa3daaa5b8ae111bbe1ef6
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2025-07-04 12:06:36 -0300

    Foi feito os metodos de Excluir e mostrar por ID na controller de livro

--- Biblioteca_WEB/src/main/java/Biblioteca_WEB/controller/livro/DeleteByIdLivro.java
+++ Biblioteca_WEB/src/main/java/Biblioteca_WEB/controller/livro/DeleteByIdLivro.java
@@ -1,8 +1,14 @@
 package Biblioteca_WEB.controller.livro;
 
+import Biblioteca_WEB.model.LivroModel;
 import Biblioteca_WEB.repository.LivroRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/livro")
@@ -12,5 +18,21 @@ public class DeleteByIdLivro {
 
     public DeleteByIdLivro(LivroRepository livroRepository) {
         this.livroRepository = livroRepository;
+    }
+
+    @GetMapping("/excluir/{id}")
+    public ModelAndView deletar(@PathVariable Long id)
+    {
+        Optional<LivroModel>livroID=this.livroRepository.findById(id);
+        if(livroID.isEmpty())
+        {
+            return new ModelAndView("redirect:/home/livro404");
+        }
+        else
+        {
+            LivroModel livroDel=livroID.get();
+            this.livroRepository.delete(livroDel);
+            return new ModelAndView("redirect:/home/index");
+        }
     }
 }
--- Biblioteca_WEB/src/main/java/Biblioteca_WEB/controller/livro/GetByIdLivro.java
+++ Biblioteca_WEB/src/main/java/Biblioteca_WEB/controller/livro/GetByIdLivro.java
@@ -1,8 +1,14 @@
 package Biblioteca_WEB.controller.livro;
 
+import Biblioteca_WEB.model.LivroModel;
 import Biblioteca_WEB.repository.LivroRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/livro")
@@ -12,5 +18,25 @@ public class GetByIdLivro {
 
     public GetByIdLivro(LivroRepository livroRepository) {
         this.livroRepository = livroRepository;
+    }
+
+    @GetMapping("/detalhe/{id}")
+    public ModelAndView showDetalhes(@PathVariable Long id)
+    {
+        Optional<LivroModel>livroID=this.livroRepository.findById(id);
+        if(livroID.isEmpty())
+        {
+            return new ModelAndView("redirect:/home/livro404");
+        }
+        else
+        {
+            LivroModel livroRecId=livroID.get();
+
+            ModelAndView mv = new ModelAndView("livro/showById");
+            mv.addObject("idLivro",livroRecId);
+            mv.addObject("BiblioResp",livroRecId.getBibliotecarioModel());
+
+            return mv;
+        }
     }
 }
